91165eaa808df4da30b0efbebead67a7e3fc61eb
Solve <solveatwork@gmail.com>
2023-11-23 19:46:26 +0900

    Separate Time Elements

--- src/main.js
+++ src/main.js
@@ -4,13 +4,33 @@
 function updateDateTime() {
   // create a new `Date` object
   const now = new Date();
+  const nowHour = formatZero(now.getHours());
+  const nowMinute = formatZero(now.getMinutes());
+  const nowSecond = formatZero(now.getSeconds());
+
+  const nowDate = now.toLocaleDateString([], {
+    weekday: 'short',
+    day: '2-digit',
+    month: 'long',
+  });
 
   // get the current date and time as a string
   const currentDateTime = now.toLocaleString();
 
   // update the `textContent` property of the `span` element with the `id` of `datetime`
-  document.querySelector('#datetime').textContent = currentDateTime;
+  document.querySelector('#date').textContent = nowDate;
+  document.querySelector('#hour').textContent = nowHour;
+  document.querySelector('#minute').textContent = nowMinute;
+  document.querySelector('#second').textContent = nowSecond;
 }
 
-// call the `updateDateTime` function every second
-setInterval(updateDateTime, 1000);
+startInterval(updateDateTime);
+
+function formatZero(number) {
+  return parseInt(number) < 10 ? '0' + number : number;
+}
+
+function startInterval(callback) {
+  callback();
+  return setInterval(callback, 1000);
+}
